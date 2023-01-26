0ad0c01dc7df9bfd04f0b7104ee0ab7897d68060
Brunno Rocha <brunnorch@hotmail.com>
2023-01-26 12:07:08 -0300

    API
    consumindo e tratando dados da API ViaCEP

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# js-consumindo-dados-api
-Repositório do treinamento "JavaScript: Consumindo e tratando dados de uma API" da Alura Cursos Online.
+# Consumindo e tratando dados da API ViaCEP
+
+### API utilizada para aprender sobre EventLoop, CallStack, TaskQueue e callback. Além do método fetch entendendo a promise e utilizando a função then, catch e finally.<br>
+### Outra maneira para acessar código assíncrono é o async await e a promise.all. Porém existe diferenças, onde o async await tem o processamento sequencial e não é tão flexivel como o then, que tem o processamento paralelo, ou seja, requisitando multiplas promises.
+
+#### JavaScript: consumindo e tratando dados de uma API com a Alura.
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => console.log(r));
+async function endereco(cep) {
+    var msgError = document.getElementById('erro');
+    msgError.innerHTML = '';
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var conversao = await consultaCep.json();
+        if (conversao.erro) {
+            throw Error('Cep não existe')
+        }
+        var endereco = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var cidade = document.getElementById('cidade');
+        var estado = document.getElementById('estado');
+
+        endereco.value = conversao.logradouro;
+        bairro.value = conversao.bairro;
+        cidade.value = conversao.localidade;
+        estado.value = conversao.uf;
+
+        console.log(conversao)
+        return conversao
+    } catch (error) {
+        console.log(error)
+        msgError.innerHTML = `<p>CEP ${cep} não existe! </p>`
+    }
+}
+
+/* 
+--------------- RESOLVENDO MULTIPLAS REQUISIÇÕES EM UMA API
+let ceps = ['44032402', '44066584'];
+let conjuntoCeps = ceps.map(valores => endereco(valores))
+Promise.all(conjuntoCeps).then(resposta => console.log(resposta)); 
+*/
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => endereco(cep.value));
+
+
+
